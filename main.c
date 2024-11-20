06d44cc99b933e1218c3f278448f4ab051b647a5
Jim <user@gmail.com>
2024-11-20 14:42:19 +0200

    Initial Commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    printf("Hello World !!!!");
+    printf("JIM zzz");
+    
+    return 0;
+}
